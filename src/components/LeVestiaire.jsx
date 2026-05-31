7a7f5f88d5d6f2cfdfd478ee00dafac9ce85c946
jpvt <akkachik@gmail.com>
2026-05-31 11:09:50 +0200

    up vestaire

--- src/components/LeVestiaire.jsx
+++ src/components/LeVestiaire.jsx
@@ -143,12 +143,9 @@ function ChatMiniLigue({ groupe, userId }) {
                 </div>
                 <span style={{ fontSize: 10, color: 'var(--text-3)', flexShrink: 0, whiteSpace: 'nowrap' }}>
                   {(() => {
-                    const d = new Date(msg.cree_le + 'Z') // force UTC parsing
-                    const auj = new Date()
-                    const memeJour = d.toDateString() === auj.toDateString()
-                    return memeJour
-                      ? d.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })
-                      : d.toLocaleDateString('fr-FR', { weekday: 'short', day: '2-digit', month: '2-digit' })
+                    const d = new Date(msg.cree_le + 'Z')
+                    return d.toLocaleDateString('fr-FR', { day: '2-digit', month: '2-digit' })
+                      + ' ' + d.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })
                   })()}
                 </span>
               </div>
