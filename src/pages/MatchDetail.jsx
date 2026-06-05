e0663906c8cd0e71daa1bf16037cb90cfb11957d
jpvt <akkachik@gmail.com>
2026-06-05 09:42:21 +0200

    up

--- src/pages/MatchDetail.jsx
+++ src/pages/MatchDetail.jsx
@@ -451,6 +451,7 @@ function MatchDetail() {
           </div>
         )}
 
+        <div style={{ height: 8 }} />
         <div style={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
 
           {!termine && prediction && (
