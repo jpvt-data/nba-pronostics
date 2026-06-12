346a766ac711e248f37824eabe142b498db51900
jpvt <akkachik@gmail.com>
2026-06-12 14:53:35 +0200

    up accueil fleches

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -455,7 +455,7 @@ function Accueil() {
         )}
 
         {!chargement && (
-          <div style={{ marginTop: 10, position: 'relative' }}>
+          <div style={{ marginTop: 10 }}>
             {matchs.length === 0 ? (
               <div style={{
                 margin: '8px 16px', padding: '14px 16px',
